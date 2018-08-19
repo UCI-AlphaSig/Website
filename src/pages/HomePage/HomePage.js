98755f963563d01fcaffdeb66557c4379880fed5
LunaPC <123.daniel.luna@gmail.com>
2018-08-19 09:18:24 -0700

    testing out carousel

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./HomePage.css";
+import {Carousel} from "react-materialize";
 
 class HomePage extends Component {
-  render(){
+  render() {
     return (
       <div>
-        THIS IS THE HOME PAGE
+        <Carousel options={{ fullWidth: true, indicators: true }}>
+          <div className='red'>
+            <h2>First Panel</h2>
+            <p className='white-text'>This is your first panel</p>
+          </div>
+          <div className='amber'>
+            <h2>Second Panel</h2>
+            <p className='white-text'>This is your second panel</p>
+          </div>
+          <div className='green'>
+            <h2>Third Panel</h2>
+            <p className='white-text'>This is your third panel</p>
+          </div>
+          <div className='blue'>
+            <h2>Fourth Panel</h2>
+            <p className='white-text'>This is your fourth panel</p>
+          </div>
+        </Carousel>
       </div>
     );
   };
